de14910273625285694da7091236f66f11dd2b78
Anna <aanikonova15@gmail.com>
2022-02-08 23:32:11 +0300

    Add Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа.
+
+Console.WriteLine("Введите трехзначное число ");
+
+int num = int.Parse(Console.ReadLine());
+
+int n1 = num / 100;
+int n2 = num % 10;
+
+Console.Write("Результат " );
+Console.Write(n1);
+Console.Write(n2);
