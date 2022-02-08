f436aeece65e502fab85eb716f5f2e989f3ad4e4
Anna <aanikonova15@gmail.com>
2022-02-08 22:56:10 +0300

    Add Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать вторую цифру трёхзначного числа.
+
+Console.WriteLine("Введите трехзначное число ");
+
+int num = int.Parse(Console.ReadLine());
+
+int n = num % 100;
+int a = n / 10;
+
+Console.WriteLine("Вторая цифра трехзначного числа - " + a);
+
